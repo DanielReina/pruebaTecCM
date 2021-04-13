56d8bcf15bae52f90807e7872da76bc7e2247520
Daniel Reina Ramos <drrmovil@gmail.com>
2021-04-13 18:06:51 +0200

    empezando el buscador

--- src/Tables.js
+++ src/Tables.js
@@ -2,55 +2,55 @@ const tables = [
   {
     table: "Mesa 1",
     _id: "m1",
-    title: "Mesa 1 - 4 comensales",
+    title: "Mesa 1 - 4 PAX",
     information: "Lorem ipsum dolor sit amet consectetur.",
   },
   {
     table: "Mesa 2",
     _id: "m2",
-    title: "Mesa 2 - 5 comensales",
+    title: "Mesa 2 - 5 PAX",
     information:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod recusandae eaque illum facere, optio temporibus!",
   },
   {
     table: "Mesa 3",
     _id: "m3",
-    title: "Mesa 3 - 2 comensales",
+    title: "Mesa 3 - 2 PAX",
     information:
       "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia, quod.",
   },
   {
     table: "Mesa 4",
     _id: "m4",
-    title: "Mesa 4 - 4 comensales",
+    title: "Mesa 4 - 4 PAX",
     information:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum autem cupiditate iusto nihil incidunt, repellat modi reprehenderit?",
   },
   {
     table: "Mesa 5",
     _id: "m5",
-    title: "Mesa 5 - 3 comensales",
+    title: "Mesa 5 - 3 PAX",
     information:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, reprehenderit?",
   },
   {
     table: "Mesa 6",
     _id: "m6",
-    title: "Mesa 6 - 4 comensales",
+    title: "Mesa 6 - 4 PAX",
     information:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, consequatur.",
   },
   {
     table: "Mesa 7",
     _id: "m7",
-    title: "Mesa 7 - 6 comensales",
+    title: "Mesa 7 - 6 PAX",
     information:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum magnam consequuntur voluptates!",
   },
   {
     table: "Mesa 8",
     _id: "m8",
-    title: "Mesa 8 - 5 comensales",
+    title: "Mesa 8 - 5 PAX",
     information:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti, optio.",
   },
--- src/components/TableList.js
+++ src/components/TableList.js
@@ -1,9 +1,8 @@
-import React,{Fragment} from "react";
+import React, { Fragment, useState } from "react";
 import styled from "styled-components";
 import tablesArray from "../Tables";
 import { makeStyles } from "@material-ui/core/styles";
-import { Popover, Typography } from "@material-ui/core";
-
+import { Popover, Typography, TextField } from "@material-ui/core";
 
 const List = styled.li`
   margin: 5px 0px;
@@ -13,93 +12,84 @@ const List = styled.li`
   text-align: center;
   line-height: 40px;
 
-&:hover{
-    background-color:red;
-}
+  &:hover {
+    background-color: red;
+  }
 `;
 
 const UList = styled.ul`
-margin-top:50px;
-`
-
-
+  margin-top: 50px;
+`;
 
 const useStyles = makeStyles((theme) => ({
   typography: {
     padding: theme.spacing(2),
   },
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+      width: "25ch",
+    },
+  },
 }));
 
-
 const TableList = ({ tables, setTable }) => {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = useState(null);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const open = Boolean(anchorEl);
+  const id = open ? "simple-popover" : undefined;
     
-     const classes = useStyles();
-     const [anchorEl, setAnchorEl] = React.useState(null);
 
-     const handleClick = (event) => {
-       setAnchorEl(event.currentTarget);
-     };
 
-     const handleClose = () => {
-       setAnchorEl(null);
-     };
 
-     const open = Boolean(anchorEl);
-    const id = open ? "simple-popover" : undefined;
-    
   return (
-    <UList>
-      {tablesArray.map((object, index) => (
-        <Fragment key={object.table}>
-          <List
-            id={object._id}
-            aria-describedby={id}
-            variant="contained"
-            color="primary"
-            onClick={handleClick}
-            key={index}
-          >
-            <p>{object.table}</p>
-          </List>
-          <Popover
-            id={id}
-            open={open}
-            anchorEl={anchorEl}
-            onClose={handleClose}
-            anchorOrigin={{
-              vertical: "bottom",
-              horizontal: "center",
-            }}
-            transformOrigin={{
-              vertical: "top",
-              horizontal: "center",
-            }}
-            key={object._id}
-          >
-            <Typography className={classes.typography} key={object.title}>
-              {object.title}
-            </Typography>
-          </Popover>
-        </Fragment>
-      ))}
-    </UList>
+    <>
+      <form className={classes.root} noValidate autoComplete="off">
+        <TextField id="outlined-basic" label="Buscar mesa" variant="outlined" />
+      </form>
+      <UList>
+        {tablesArray.map((object) => (
+          <Fragment key={object._id}>
+            <List
+              id={object._id}
+              aria-describedby={id}
+              variant="contained"
+              color="primary"
+              onClick={handleClick}
+            >
+              <p>{object.table}</p>
+            </List>
+            <Popover
+              id={id}
+              open={open}
+              anchorEl={anchorEl}
+              onClose={handleClose}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "center",
+              }}
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "center",
+              }}
+            >
+              <Typography id="pop" className={classes.typography}>
+                <strong>Lorem ipsum, dolor sit amet</strong>
+                <br />
+                Consectetur adipisicing elit.
+              </Typography>
+            </Popover>
+          </Fragment>
+        ))}
+      </UList>
+    </>
   );
 };
 
 export default TableList;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
